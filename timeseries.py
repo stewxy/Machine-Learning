0c9aa575e7dc86849db7d1251b44c007c6005b7c
Stephen <stewxy13@gmail.com>
2023-07-26 20:05:22 +1200

    cleaned up code

--- timeseries.py
+++ timeseries.py
@@ -17,7 +17,6 @@ import seaborn as sns
 
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.tsa.arima.model import ARIMA
-#from statsmodels.tsa.arima_model import ARMA
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
@@ -43,32 +42,28 @@ pred_start_date = test.index[0]
 pred_end_date = test.index[-1]
 
 #=========================ARMA=========================
-#model, 
-#(p: number of autoregressive terms(AR order), d:number of nonseasonal differences(differencing order), q:number of moving-average terms(MA order))
+#model(p: number of autoregressive terms(AR order), d:number of nonseasonal differences(differencing order), q:number of moving-average terms(MA order))
 ARMAmodel=SARIMAX(train, order=(1, 0, 1))
 model_fit = ARMAmodel.fit()
-#print("*******MODEL FIT*******", model_fit.summary())
-#print("*******Head*******", btc.head())
 
 ARMApredictions = model_fit.predict(start=pred_start_date, end=pred_end_date)
-
-
-#residuals = test-predictions
-#print("*************",residuals)
-#print("*************",predictions)
-#plt.plot(residuals)
-
-#Calculating Root Mean Square Error(RMSE) of testing data compared to predictions, higher number = worse
+'''
+residuals = test-predictions
+print("**********RESIDUALS**********",residuals)
+plt.plot(residuals)
+'''
+#RMSE(Root Mean Square Error) of testing data compared to predictions, higher number = worse
 arma_rmse = numpy.sqrt(mean_squared_error(test, ARMApredictions))
 print("ARMA RMSE: ",arma_rmse)
 plt.plot(ARMApredictions, color="green", label="ARMA Predictions")
+
 #=========================ARIMA=========================
 ARIMAmodel = SARIMAX(train, order=(2,2,2))
 ARIMAmodel_fit = ARIMAmodel.fit()
 ARIMApredictions = ARIMAmodel_fit.predict(start=pred_start_date, end=pred_end_date)
 
-#arima_rmse = numpy.sqrt(mean_squared_error(test, ARIMApredictions))
-#print("ARIMA RMSE: ",arima_rmse)
+arima_rmse = numpy.sqrt(mean_squared_error(test, ARIMApredictions))
+print("ARIMA RMSE: ",arima_rmse)
 
 plt.plot(ARIMApredictions, color="orange", label="ARIMA Predictions")
 
@@ -85,77 +80,24 @@ plt.plot(SARIMApredictions, color="red", label="SARIMA Predictions")
 pred = model_fit.get_forecast(len(test.index))
 predictions = pred.conf_int(alpha = 0.05) 
 predictions["p"] = model_fit.predict(start=pred_start_date, end=pred_end_date)
-#residuals = test-predictions
-#print("============",residuals)
-
 
 predout = predictions["p"] 
 print("==========", predout)
 
-#plt.plot(residuals)
 plt.plot(predout, color="green", label="Predictions")
 '''
 #plt.axhline(20000, color="r", linestyle="--", alpha=0.5)
 
+#Graph Labels
 plt.legend()
-
-
-#sns.set()
-#graph labels
 plt.ylabel('BTC Price')
 plt.xlabel('Date')
-#styling
+
+#Styling
 plt.xticks(rotation=45)
 plt.plot(train, color='black', label="Training")
 plt.plot(test, color='red', label="Testing")
-#plt.plot(btc.index, btc)
 
 plt.show()
-
-'''
-x = [1,2,3,4,5,6,7,8,9,10]
-y = [10,15,20,25,30,25,30,35,40,50]
-
-mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
-
-myline = numpy.linspace(1, 10, 50)
-
-plt.scatter(x, y)
-plt.plot(myline, mymodel(myline))
-plt.show()
-
-
-#training and testing data, train 80%, test 20%
-train_x = x[:80]
-train_y = y[:80]
-test_x = x[80:]
-test_y = y[80:]
-'''
-
-
-'''
-slope, intercept, r, p, std_err = stats.linregress(x, y)
-
-#0 = no relationship, 1/-1 = full relationship
-print("Relationship: ", r)
-
-def linearregression(x):
-    return slope * x + intercept
-mymodel = list(map(linearregression, x))
-'''
-
-
-'''
-#x, y axis labels and x label rotation
-plt.ylabel('Y')
-plt.xlabel('X')
-plt.xticks(rotation=45)
-
-plt.scatter(train_x, train_y)
-plt.plot(x, mymodel)
-plt.show()
-
-
-'''
 plt.savefig(sys.stdout.buffer)
 sys.stdout.flush()
